86d638ebb491af1cee21bd424f6e51dd5440e58b
communal-tutorial-3267 <jrodden1@gmail.com>
2018-12-18 03:21:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function shout(string) {
+var uppercase = ''
+var lowercase = ''
 
+
+function shout(string) {
+  uppercase = string.toUpperCase();
+  return uppercase
 }
 
 function whisper(string) {
-
+  lowercase = string.toLowerCase();
+  return lowercase
 }
 
 function logShout(string) {
-
+  console.log(uppercase)
 }
 
 function logWhisper(string) {
-
+  console.log(lowercase)
 }
 
 function sayHiToGrandma(string) {
